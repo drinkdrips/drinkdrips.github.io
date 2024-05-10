2417511f19565124a571deb1f134710bb17fc63a
drinkdrips <153828385+drinkdrips@users.noreply.github.com>
2024-05-10 10:18:13 -0300

    Create roadmap.js

---
+++ roadmap.js
@@ -0,0 +1,39 @@
+/*
+   ES6
+   Forget about jQuery
+*/
+
+// Expand/Collapse Article
+document.querySelectorAll("#infographic article").forEach((article) => {
+   article.addEventListener("click", () => {
+      article.classList.toggle("active");
+   });
+});
+
+// Always Collapse Article on click outside
+document.addEventListener("mouseup", (e) => {
+   document.querySelectorAll("article.active").forEach((article) => {
+      if (article.contains(e.target)) return;
+      if (article === e.target) return;
+      article.classList.remove("active");
+   });
+});
+
+// Activate artciles through prev/next interactions
+document.querySelectorAll("#infographic article .roadmapBtn").forEach((roadmapBtn) => {
+   roadmapBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      var isprev =
+         e.target === e.target.parentElement.firstElementChild ? true : false;
+      var article = roadmapBtn.closest("article");
+      var step = parseInt(article.getAttribute("data-step"));
+      var next = document.querySelector(
+         `[data-step="${isprev ? step - 1 : step + 1}"]`
+      );
+      next.classList.add("active");
+      next.scrollIntoView({
+         behavior: "smooth",
+         block: "nearest"
+      });
+   });
+});
