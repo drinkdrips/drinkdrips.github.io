8f5453249b59e0f625303a23c9323e1a200a87ad
DrinkDrips <153828385+drinkdrips@users.noreply.github.com>
2024-08-21 08:48:52 +0100

    Update roadmap.js

--- roadmap.js
+++ roadmap.js
@@ -1,3 +1,31 @@
+const iconBoxes = document.querySelectorAll(".menu-item");
+const closeBtns = document.querySelectorAll(".close-btn");
+const body = document.querySelector("body");
+
+function toggleModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "block";
+        body.classList.add("prevent-background-scroll");
+    }
+}
+
+closeBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        let modal = btn.closest(".popup");
+        modal.style.display = "none";
+        body.classList.remove("prevent-background-scroll");
+    });
+});
+
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("popup")) {
+        e.target.style.display = "none";
+        body.classList.remove("prevent-background-scroll");
+    }
+});
+
+// Script ROADMAP
 /*
    ES6
    Forget about jQuery
